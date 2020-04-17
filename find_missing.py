077b5abb13b5627fe72dc386a7011cf0051ea243
Naouali <elnaoualinebil@gmail.com>
2020-04-17 12:39:29 +0100

    sets

--- find_missing.py
+++ find_missing.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 #find the missing element 
-#note 20% time to accomplish 10mincl
+#note 20% time to accomplish 10mincl --> without using sets
+#second note 50% with sets
 def solution(A):
     a = sorted(A)
+    a = set(a)
     mini = min(a)
     maxi = max(a)
     my_list = sorted(list(range(mini, maxi + 1)))
-    for i in my_list:
+    for i in set(my_list):
         if i not in a:
             return i
 

b8e3344eea206cd453d52689f46b132811c936a8
Naouali <elnaoualinebil@gmail.com>
2020-04-18 15:14:48 +0100

    swap

--- single_swap.py
+++ single_swap.py
@@ -8,17 +8,11 @@ def solution(a, n):
         return 1
     mylist = []
     for x, y in zip(a,k):
-        mylist.append(x - y)
-    for i in mylist:
-        if i != 0:
-            count += 1
-    if count == 2:
+        if x - y != 0:
+            mylist.append(x - y)
+    if len(mylist) == 2:
         return 1
     return 0
 
-
-
 a = [1,5,3,3,7]
-b = [1,3,5,3,4]
-c = [1,3,5]
-print(solution(c,5))
+print(solution(a,5))
--- whole_square.py
+++ whole_square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def solution(a,b):
+def solution(A):
+    def solution(a,b):
     count = 0
     l = set(range(a, b + 1))
     sqr = set(range(1, b + 1))
@@ -8,6 +9,7 @@ def solution(a,b):
             count+=1
     return count
 
-
-
-print(solution(-2,19))
+a =  [7, 2, 3, 6, 5, 9, 1, 4, 8]
+c = [1, 2, 3, 4, 6, 7, 8, 9, 10]
+print(solution(a))
+print(solution(c))
